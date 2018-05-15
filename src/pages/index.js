cb6d756bcc72ff0e9890f9f5d7624a00da490076
brianbuie <brianjamesbuie@gmail.com>
2018-05-14 19:42:43 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default () => (
+  <Container>
+    <h1>Neato</h1>
+  </Container>
+);
