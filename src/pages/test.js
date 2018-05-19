9533ed595aee8b2b41a7c3e210f6d322e93fcb5a
brianbuie <brianjamesbuie@gmail.com>
2018-05-19 18:40:39 -0500

    added test page for link demo

--- src/pages/test.js
+++ src/pages/test.js
@@ -15,8 +15,8 @@ const Container = styled.div`
 export default () => (
   <Layout>
     <Container>
-      <h1>Neato</h1>
-      <p><Link to="/test">Test Page</Link></p>
+      <h1>Test Page</h1>
+      <p><Link to="/">Home</Link></p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </Container>
   </Layout>
